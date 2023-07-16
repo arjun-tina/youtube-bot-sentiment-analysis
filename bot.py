5689a31b1ad50879568c499b118895db295a20b6
Arjun Chadha <arjun.chadha@duke.edu>
2023-07-16 16:47:56 -0400

    Updated authentication, added comment scraping mechanism

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 #import libraries and packages
-import googleapiclient
 import googleapiclient.discovery
 import googleapiclient.errors
 from textblob import TextBlob
@@ -10,7 +9,18 @@ import re
 #authentication
 api_service_name = "youtube"
 api_version = "v3"
-youtube_api_key = "insert your key here"
+youtube_api_key = "insert your YouTube API key here"
 
-api = googleapiclient.discovery.build(api_service_name, api_version, youtube_api_key)
+api = googleapiclient.discovery.build(api_service_name, api_version, developerKey=youtube_api_key)
 
+# request comments from desired YouTube video
+channel = "insert channel name here"
+id = "insert video ID here"
+
+request = api.commentThreads().list(part="snippet", videoId=id, maxResults=4)
+response = request.execute()
+
+#add comments to comments list
+comments = []
+for item in response['items']:
+    comments.append(item['snippet']['topLevelComment']['snippet']['textDisplay'])
