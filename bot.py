d9b430b404cb24eaeef8c2ac91c7c48a159cfe85
Arjun Chadha <arjun.chadha@duke.edu>
2023-07-16 14:27:48 -0400

    Updated imported libraries and packages, added YouTube API authentication

--- bot.py
+++ bot.py
@@ -1,8 +1,16 @@
 #import libraries and packages
-from googleapiclient import discovery, errors
+import googleapiclient
+import googleapiclient.discovery
+import googleapiclient.errors
 from textblob import TextBlob
 import csv
 import os
 import re
 
+#authentication
+api_service_name = "youtube"
+api_version = "v3"
+youtube_api_key = "insert your key here"
+
+api = googleapiclient.discovery.build(api_service_name, api_version, youtube_api_key)
 
